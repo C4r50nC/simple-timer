0280510cbc42daf362c96c9311a79004a0ef96ef
C4r50nC <xichen059@gmail.com>
2024-03-16 19:51:36 +1100

    fix: fix timer not refreshing on pop-up page

--- popup.js
+++ popup.js
@@ -2,13 +2,18 @@ const currentTimeElement = document.getElementById("current-time");
 const nameElement = document.getElementById("user-name");
 const timerElement = document.getElementById("timer-text");
 
-chrome.storage.local.get(["timerInSeconds"], (res) => {
-  const timeInSeconds = res.timerInSeconds ?? 0;
-  timerElement.textContent = `The timer is at: ${timeInSeconds} seconds`;
-});
+function updateTimerElement() {
+  chrome.storage.local.get(["timerInSeconds"], (res) => {
+    const timeInSeconds = res.timerInSeconds ?? 0;
+    timerElement.textContent = `The timer is at: ${timeInSeconds} seconds`;
+  });
 
-const currentTime = new Date().toLocaleTimeString();
-currentTimeElement.textContent = `The current time is: ${currentTime}`;
+  const currentTime = new Date().toLocaleTimeString();
+  currentTimeElement.textContent = `The current time is: ${currentTime}`;
+}
+
+updateTimerElement();
+setInterval(updateTimerElement, 1000);
 
 chrome.storage.sync.get(["name"], (res) => {
   const name = res.name ?? "???";
